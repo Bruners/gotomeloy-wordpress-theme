5daa5a85bf95821935d9c7e5ee8de4c3eb58628f
Lasse Brun <bruners@gmail.com>
2016-02-25 16:46:12 +0100

    Add more mobile styles to omoss.php

--- omoss.php
+++ omoss.php
@@ -2,76 +2,76 @@ GO TO MELØY består av flere reiselivs-operatører som tilhører Meløy og omhe
 
 &nbsp;
 <div class="row">
-	<div class="col-sm-9"><strong>Meløy Adventure</strong>
+	<div class="col-sm-12 col-md-9"><strong>Meløy Adventure</strong>
 		Glomveien 62
 		8160 Glomfjord
 		+47 915 40 866
 		<a href="http://www.meloyadventure.no/" target="_blank">meloyadventure.no</a> | <a href="mailto:post@meloyadventure.no">post@meloyadventure.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1279" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/meloyadventure_logo_110x72.png" alt="meloyadventure_logo_110x72" width="110" height="72" />
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-9"><strong>Støtt Brygge</strong>
+	<div class="col-sm-12 col-md-9"><strong>Støtt Brygge</strong>
 		8159 Støtt
 		+47 400 21 212
 		<a href="http://www.stott.no/" target="_blank">stott.no</a> | <a href="mailto:eaa@stott.no">eaa@stott.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1280" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/stottbrygge_logo_110x110.png" alt="stottbrygge_logo_110x110" width="110" height="110" />
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-9"><strong>Glomfjord Hotell AS</strong>
+	<div class="col-sm-12 col-md-9"><strong>Glomfjord Hotell AS</strong>
 		Lars Evensens vei 3
 		8160 Glomfjord
 		+47 757 52 500
 		<a href="http://www.glomfjordhotell.no/" target="_blank">glomfjordhotell.no</a> | <a href="mailto:info@glomfjordhotell.no">info@glomfjordhotell.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1281" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/glomfjord_hotell_110x54.png" alt="glomfjord_hotell_110x54" width="110" height="54" />
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-9"><strong>Bodø Hotell AS</strong>
+	<div class="col-sm-12 col-md-9"><strong>Bodø Hotell AS</strong>
 		Professor Schyttes gate 5
 		8006 Bodø
 		+47 755 47 700
 		<a href="http://www.bodohotell.no/" target="_blank">bodohotell.no</a> | <a href="mailto:booking@bodohotell.no">booking@bodohotell.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1282" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/bodohotell_logo_110x110.png" alt="bodohotell_logo_110x110" width="110" height="110" />
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-9"><strong>Rocks &amp; Rivers</strong>
+	<div class="col-sm-12 col-md-9"><strong>Rocks &amp; Rivers</strong>
 		Vallsjøen
 		8170 Engavågen
 		+47 41 08 29 81
 		<a href="http://www.rocksandrivers.no/" target="_blank">rocksandrivers.no</a> | <a href="mailto:post@rocksandrivers.no">post@rocksandrivers.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1283" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/rocksnrivers_logo_110x58.png" alt="rocksnrivers_logo_110x58" width="110" height="58" />
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-9"><strong>Ørnes Hotell AS</strong>
+	<div class="col-sm-12 col-md-9"><strong>Ørnes Hotell AS</strong>
 		Havneveien 12
 		8150 Ørnes
 		+47 75 75 45 99
 		<a href="http://www.orneshotell.no" target="_blank">orneshotell.no</a> | <a href="mailto:bjorn@orneshotell.no">bjorn@orneshotell.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1284" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/01/orneshotell_logo_110x52.png" alt="orneshotell_logo_110x52" width="110" height="52" />
 	</div>
-	<div class="col-sm-9"><strong>Sixt Bilutleie</strong>
+	<div class="col-sm-12 col-md-9"><strong>Sixt Bilutleie</strong>
 		Hålogalandsgata 7
 		8006 Bodø
 		+47 755 40 100
 		<a href="https://www.sixt.no/bilutleie/norge/bodoe/bodoe" target="_blank">sixt.no</a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-12 col-md-3">
 		<img class="alignnone size-full wp-image-1286" src="http://www.gotomeloy.no/wordpress/wp-content/uploads/2016/02/sixt_logo_110x57.png" alt="sixt_logo_110x57" width="110" height="57" />
 	</div>
 </div>
