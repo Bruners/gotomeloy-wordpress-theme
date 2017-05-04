dabe3c503c0b8ee5eed2f9d7cff5b155f00133d3
Lasse Brun <bruners@gmail.com>
2017-05-04 14:11:19 +0200

    Clean up facebook events

--- template-stott.php
+++ template-stott.php
@@ -427,11 +427,7 @@ Template Name: Portfolio Stott
                                 $event_title = get_the_title();
                                 $event_image = get_fbe_image('cover');
                                 $event_url =  get_fbe_field('fb_event_uri');
-                                $event_facebook = get_fbe_field('facebook');
-                                $event_string = array('https://facebook.com','/');
-                                $event_string2 = array('','');
-                                $event_site = str_replace($event_string, $event_string2, $event_facebook);
-                                $event_site = ucfirst($event_site);
+                                $event_site = ucfirst(str_replace(array('https://facebook.com','/'), array('',''), get_fbe_field('facebook') ));
                                 $event_location = get_fbe_field('location');
                                 $event_starts_month = get_fbe_date('event_starts','M');
                                 $event_starts_day = get_fbe_date('event_starts','j');
