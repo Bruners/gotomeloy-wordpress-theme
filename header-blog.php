5b1d1d32f9128e4e4bf6f98b33dfdd5a49d8cdc5
Lasse Brun <bruners@gmail.com>
2017-09-01 13:57:20 +0200

    Move facebook and google scripts to separate files
    Move google analytics code into analyticstracking.php
    Move facebook code into facebooktracking.php

--- header-blog.php
+++ header-blog.php
@@ -23,22 +23,8 @@
     </head>
 
     <body <?php body_class(); ?>>
-        <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId      : '583743631778760',
-                    xfbml      : true,
-                    version    : 'v2.5'
-                });
-            };
-
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/nb_NO/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+        <?php include_once("analyticstracking.php") ?>
+        <?php include_once("facebooktracking.php") ?>
 
         <!-- BEGIN: MAIN STRUCTURE -->
         <main id="main-structure">
--- header.php
+++ header.php
@@ -28,22 +28,8 @@
         <div class="preloader">
             <div class="loader"></div>
         </div>
-        <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId      : '583743631778760',
-                    xfbml      : true,
-                    version    : 'v2.5'
-                });
-            };
-
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/nb_NO/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+        <?php include_once("analyticstracking.php") ?>
+        <?php include_once("facebooktracking.php") ?>
 
         <!-- BEGIN: MAIN STRUCTURE -->
         <main id="main-structure">
