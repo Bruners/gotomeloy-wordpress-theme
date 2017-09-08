8e24f31ff0623f6568f0a97365a3c5b31118ecec
Lasse Brun <bruners@gmail.com>
2017-09-08 13:47:25 +0200

    Use headroom.js on header
    Slide header away on scroll down and show it again on scroll up
    http://wicky.nillia.ms/headroom.js/

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
         <main id="main-structure">
 
             <!-- BEGIN: SITE HEADER -->
-            <header class="site-header">
+            <header id="header" class="headroom header header--fixed" role="banner">
                 <div class="header-inner">
                     <div class="container clearfix">
 
--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,74 @@ jQuery(document).ready(function( $ ) {
             preload: false
         }
     });
-
-    
 });
+
+/* jQuery('div.hero-header-title').click(function(){
+    video = '<iframe src="'+ jQuery('div.hero-image').attr('data-video') +'" frameborder="0" allowfullscreen></iframe>';
+    jQuery('div.hero-image').replaceWith(video);
+}); */
+
+jQuery("#stottfilm").click(function() {
+        jQuery.fancybox({
+            'padding'       : 0,
+            'autoScale'     : false,
+            'transitionIn'  : 'none',
+            'transitionOut' : 'none',
+            'title'         : this.title,
+            'width'     : 680,
+            'height'        : 495,
+            'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+            'type'          : 'swf',
+            'swf'           : {
+                 'wmode'        : 'transparent',
+                'allowfullscreen'   : 'true'
+            }
+        });
+
+    return false;
+});
+
+(function($) {
+
+    'use strict';
+    function getImage() {
+        var src = '';
+        var lastimageurl = "http://www.stott.no/wp-content/themes/gotomeloy/parts/ajax-get-webcam-image.inc.php?t="
+        jQuery.ajax({
+            url: lastimageurl  + new Date().getTime(),
+            async: false,
+            success: function(data){
+              src = data;
+            }
+        });
+        return src;
+    }
+
+    function updateImage() {
+            var src = getImage();
+            jQuery("#webcam-img").attr("src", src + "?timestamp=" + new Date().getTime());
+            jQuery("#webcam-url").attr("href", src + "?timestamp=" + new Date().getTime());
+            setTimeout(updateImage, 10000);
+        }
+
+    updateImage();
+
+})(jQuery);
+
+            (function() {
+                var header = document.querySelector("#header");
+
+                if(window.location.hash) {
+                  header.classList.add("headroom--unpinned");
+                }
+
+                var headroom = new Headroom(header, {
+                    tolerance: {
+                      down : 10,
+                      up : 20
+                    },
+                    offset : 205
+                });
+                headroom.init();
+
+            }());
